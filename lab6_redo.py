45715e7d3460f9ccc4e0922d70652b46b94f6dea
karliesanders <karliesanders@gmail.com>
2018-06-07 22:22:17 -0700

    workin'

--- lab6_redo.py
+++ lab6_redo.py
@@ -1,24 +1,16 @@
 _author_ = 'Karlie Sanders'
 
-#user_file = input("enter file name: ")
-#with open ('lab6file.txt', 'r+') as file:
-#	file_lines = file.readlines()
-#file.close()
-#for line in file_lines:
-#	print(line)
-
 
 def file_reader():
     user_file = input("enter file name: ")
     with open('lab6file.txt') as file:
-        file_lines = file.readlines()
-    print(file_lines)
+        whole_file_as_string = file.read().replace('\n', ' ')
+    print(whole_file_as_string)
     file.close()
-    for line in file_lines:
-        return(line)
+    return(whole_file_as_string)
 
 def word_counter():
-    words = file_reader.split()
+    words = file_reader().split()
     counts = {}
     for word in words:
         if word in counts.keys():
@@ -31,33 +23,3 @@ def word_counter():
 
 word_counter()
 
-'''      
-def main():
-
-   user_file = input("enter file name:(lab6file.txt) ")
-   with open('lab6file.txt') as file:
-      file_lines = file.readlines()
-      file.close()
-   for line in file_lines:
-      words = line.split()
-      counts = {}
-      for word in words:
-          if word in counts.keys():
-             counts[word] = counts[word] + 1
-          else:
-             counts[word] = 1
-
-   for key in sorted(counts.keys()):
-      print("the word {0} occurs {1} times".format(key,counts[key]))
-
-main()
-
-'''
-'''
-File_name = input(“enter file name: “)
-With open (file_name as file:
-	File_lines = file.readlines()
-file.close()
-For line in file_lines:
-	print(line)
-'''
